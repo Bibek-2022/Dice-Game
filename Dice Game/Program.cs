6385e14691c837707003b65cbdf2106ae26174d9
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-11-14 22:27:34 +1100

    'simple-dice-game'

--- Dice Game/Program.cs
+++ Dice Game/Program.cs
@@ -6,7 +6,41 @@ namespace Dice_Game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int playerRandomNumber;
+            int enemyRandomNumber;
+
+            int playerPoint = 0;
+            int enemyPoint = 0;
+            Random random = new Random();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Press any key to roll the dice");
+                Console.ReadKey();
+                playerRandomNumber = random.Next(1, 7);
+                Console.WriteLine("You rolled" + playerRandomNumber);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNumber = random.Next(1, 7);
+                Console.WriteLine("Opposition rolled" + enemyRandomNumber);
+           
+                if(playerRandomNumber > enemyRandomNumber)
+                {
+                    playerPoint = playerPoint + 1;
+                    Console.WriteLine("You win this round");
+                }  else if (playerRandomNumber < enemyRandomNumber) {
+                    enemyPoint++;
+                    Console.WriteLine("Enemy win this round");
+                } else
+                {
+                    Console.WriteLine("Its a Draw");
+                }
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
